b330631778ec7a88aa5e290b9870161fabd56b50
rana hammad <ranahamamd9795@gmail.com>
2026-06-07 17:59:12 +0500

    App  - RANA HAMMAD MUSHTAQ - bse233041

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
-import Header from './components/Header'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Login from './components/Login'
+import { useContext } from 'react'
+import { AppContext } from './context/AppContext'
 
-export default function App() {
+const App = () => {
+  const { showLogin } = useContext(AppContext)
+
   return (
     <div>
-      <Header />
-      <main>
-        <Home />
-      </main>
+      <Navbar />
+      {showLogin && <Login />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/result' element={<Result />} />
+        <Route path='/buy' element={<BuyCredit />} />
+      </Routes>
+      <Footer />
     </div>
   )
 }
+
+export default App
+
+
